b4867108a73560fd6cb9f825caea0cbbb617a4ad
Ellen Yang <yange@oregonstate.edu>
2021-02-25 21:26:19 -0800

    display backdrop in movie detail

--- flix/MovieDetailsViewController.swift
+++ flix/MovieDetailsViewController.swift
@@ -22,13 +22,22 @@ class MovieDetailsViewController: UIViewController {
 
         // Do any additional setup after loading the view.
       titleLabel.text = movie["title"] as? String
+      titleLabel.sizeToFit()
+
       synopsisLabel.text = movie["overview"] as? String
+      synopsisLabel.sizeToFit()
 
       let baseUrl = "https://image.tmdb.org/t/p/w185"
       let posterPath = movie["poster_path"] as! String
       let posterUrl = URL(string: baseUrl + posterPath)
 
       posterView.af_setImage(withURL: posterUrl!)
+
+      let backdropPath = movie["backdrop_path"] as! String
+      let backdropUrl = URL(string:"https://image.tmdb.org/t/p/w780" + backdropPath)
+
+      backdropView.af_setImage(withURL: backdropUrl!)
+
 
     }
     
