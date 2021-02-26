1567fb0c5ef74c035115df04f244aa1ebc6a1b56
Ellen Yang <yange@oregonstate.edu>
2021-02-25 23:49:55 -0800

    able to display grid view but need to work on sizing

---
+++ flix/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  flix
+//
+//  Created by Ellen Yang on 2/25/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+   @IBOutlet weak var posterView: UIImageView!
+   
+}
