24741d1da3a5563cc3725596b5fcb8ab43a72ca5
Ellen Yang <yange@oregonstate.edu>
2021-02-26 00:29:42 -0800

    turn on autoresizing

--- flix/MovieGridViewController.swift
+++ flix/MovieGridViewController.swift
@@ -19,6 +19,14 @@ class MovieGridViewController: UIViewController, UICollectionViewDataSource,UICo
       collectionView.delegate = self
       collectionView.dataSource = self
 
+      let layout = collectionView.collectionViewLayout as! UICollectionViewFlowLayout
+
+      layout.minimumLineSpacing = 4
+      layout.minimumInteritemSpacing = 4
+
+      let width = (view.frame.size.width - layout.minimumLineSpacing * 2) / 3
+      layout.itemSize = CGSize(width: width, height: width * 3/2)
+
         //Download superhero movies
       let url = URL(string: "https://api.themoviedb.org/3/movie/297762/similar?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
       let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
