4852d1267e554439956e59cab68c0ed8693ab703
Ellen Yang <yange@oregonstate.edu>
2021-05-07 13:30:58 -0700

    user can see the trailer when tap on poster

--- flix/Controllers/GridDetailViewController.swift
+++ flix/Controllers/GridDetailViewController.swift
@@ -48,12 +48,13 @@ class GridDetailViewController: UIViewController {
 
 
     // MARK: - Navigation
-/*
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: trailerViewController, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-*/
+
+   override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+      let tralierView = segue.destination as! trailerViewController
+
+      tralierView.movieID = movie["id"] as! Int
+
+   }
+
 
 }
--- flix/Controllers/trailerViewController.swift
+++ flix/Controllers/trailerViewController.swift
@@ -12,6 +12,10 @@ class trailerViewController: UIViewController, WKUIDelegate {
 
    @IBOutlet weak var webView: WKWebView!
 
+   var trailers = [[String:Any]]()
+   var movieID: Int!
+
+
    /*
    override func loadView() {
       let webConfiguration = WKWebViewConfiguration()
@@ -24,14 +28,39 @@ class trailerViewController: UIViewController, WKUIDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        getMovie()
 
-        let myURL = URL(string:"https://www.apple.com")
-        let myRequest = URLRequest(url: myURL!)
-        webView.load(myRequest)
     }
     
    @IBAction func returnButton(_ sender: Any) {
       dismiss(animated: true, completion: nil)
+   }
+
+   func getMovie(){
+      let url = URL(string: "https://api.themoviedb.org/3/movie/\(movieID!)/videos?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed&language=en-US")!
+      let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
+      let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+      let task = session.dataTask(with: request) { (data, response, error) in
+         // This will run when the network request returns
+         if let error = error {
+            print(error.localizedDescription)
+         } else if let data = data {
+            let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
+
+            self.trailers = dataDictionary["results"] as! [[String:Any]]
+
+            let key = self.trailers[0]["key"]
+            let myURL = URL(string:"https://www.youtube.com/watch?v=\(key!)")
+
+            let myRequest = URLRequest(url: myURL!)
+
+            self.webView.load(myRequest)
+
+         }
+
+      }
+      task.resume()
+
    }
 
 
